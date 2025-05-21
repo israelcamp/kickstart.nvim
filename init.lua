e5ef3fa024e337bf1b5a395ac2d1d44d93ac8d2a
Israel Campiotti <israelcampiotti@gmail.com>
2025-05-21 10:28:55 -0300

    changing themes

--- init.lua
+++ init.lua
@@ -200,6 +200,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Prettier on buffer
+vim.keymap.set('n', '<leader>p', ':!prettier --write %<CR>', { desc = 'Format current buffer with prettier' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -247,6 +250,18 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'dark',
+      }
+      -- Enable theme
+      require('onedark').load()
+    end,
+  },
+  'scottmckendry/cyberdream.nvim',
   'nvim-tree/nvim-tree.lua',
   { 'nvim-tree/nvim-web-devicons', opts = {} },
   'github/copilot.vim', -- AI pair programmer
