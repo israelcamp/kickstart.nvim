6c5e654198619e773d9c0b188d4e5228f8024271
Israel Campiotti <israelcampiotti@gmail.com>
2025-06-17 21:16:26 -0300

    copilot with control J

--- init.lua
+++ init.lua
@@ -1047,3 +1047,9 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.expandtab = true
   end,
 }) --
+
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
